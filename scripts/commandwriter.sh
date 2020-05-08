079ebfa83e609492689e923a22aab76c9c8fe179
Mahmudulla Hassan <mhassan@miners.utep.edu>
2020-05-08 12:33:04 -0500

    remove obsolete scripts

--- scripts/commandwriter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for f in /scratch/02875/docking/ligands/Enamine-PC/1/*.pdbqt; do
-	 outputfile="/scratch/03864/suman1/COVID19/docked/6w9c/AC/1/$(basename -- ${f%.*}).docked"
-	logfile="/scratch/03864/suman1/COVID19/docked/6w9c/AC/1/$(basename -- ${f%.*}).log"
-	echo /scratch/03864/suman1/COVID19/qvina/bin/qvina02 --receptor ./6w9c-A.pdbqt --ligand "$f" --center_x -14 --center_y 46 --center_z -39 --size_x 24 --size_y 24 --size_z 24 --out $outputfile --log $logfile
-done
--- scripts/vinalogscoresort.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for f in ./docked/6w9c/AC/1/logs/*.log; do
-   score=$(grep -m 1 '   1   ' $f | awk '{print $2}')
-   echo $(basename $f .log),$score
-   #mv $f 5r80_e/logs/part3/parsed/
-done > scores.csv
-# sort
-sort -t, -nk2 scores.csv > sorted_scores.csv
